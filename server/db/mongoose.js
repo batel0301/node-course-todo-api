8e1509696150c805c8f6176169db9903694bf090
Bat-el Pinto <bat-el.pinto@sharegain.com>
2019-03-06 17:19:29 +0200

    added useNewUrlParser on mongo connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 //mongoose config
 mongoose.Promise = global.Promise;
-mongoose.connect(  process.env.MONGODB_URI|| 'mongodb://localhost:27017/TodoApp');
+mongoose.connect( process.env.MONGODB_URI|| 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 module.exports = { mongoose};
