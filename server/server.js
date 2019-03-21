950659960057479a58f3f3fd8d9497bd1158c531
Bat-el Pinto <bat-el.pinto@sharegain.com>
2019-03-21 12:01:11 +0200

    middlewares and authentication

--- server/server.js
+++ server/server.js
@@ -5,11 +5,12 @@ var bodyParser = require('body-parser');
 const {ObjectId} = require('mongodb');
 
 
+
 //local 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo}  = require('./models/todo.js');
 var {User}  = require('./models/user.js');
-
+var {authenticate} = require('./middleware/authenticate.js');
 
 var app =express();
 const port = process.env.PORT ;
@@ -42,6 +43,12 @@ app.get('/todos',(req,res)=>{
         })
 })
 
+
+
+app.get('/users/me',authenticate, (req,res)=> {
+res.send(req.user);
+
+});
 //GET 'todos/id
 
 app.get('/todos/:id', (req,res)=>{
@@ -95,9 +102,39 @@ app.patch('/todos/:id', (req,res)=>{
     });
 })
 
+
+
+
+//POST /users
+//use pick like on patch
+
+
+app.post('/users',(req,res)=>{
+    var body = _.pick(req.body, ['email', 'password']);
+
+    var user = new User(body);
+    
+
+    user.save().then(()=> {
+        return user.generateAuthToken();
+    }).then((token)=>{
+        res.header('x-auth',token).send(user);
+    })
+    .catch((e)=> {
+        console.log(e);
+        res.status(400).send(e);
+    })
+
+
+});
+
+
+
+
+
 app.listen(port,()=>{
     console.log(`Started on port ${port}`);
-    console.log(`environment ${env}`);
+   
 });
 
 
